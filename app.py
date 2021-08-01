828eb0a550187b5d1774dc69293e9376f8138a35
xfajardo06 <88059144+xfajardo06@users.noreply.github.com>
2021-08-01 03:03:03 -0500

    Update app.py
    Add scroll down

--- app.py
+++ app.py
@@ -3,35 +3,79 @@ import requests
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
 app = Flask(__name__)
+
 
 @app.route("/", methods = ['GET'])
 def get_dato():
-    url_computers = urlopen("https://webscraper.io/test-sites/e-commerce/scroll/computers")
-    url_phones = urlopen("https://webscraper.io/test-sites/e-commerce/scroll/phones")
-    datoC = list()
-    computerObj = BeautifulSoup(url_computers, 'html.parser')
-    datosComputer = computerObj.findAll("div", {"class":"caption"})
-    for datoComputer in datosComputer:
-        datoC.append({
-            "nombre": datoComputer.find("a", {"class":"title"}).text,
-            "caracteristicas": datoComputer.find("p", {"class":"description"}).text,
-            "valor":datoComputer.find("h4", {"class":"price"}).text
-         })
-    datoP = list()
-    phonesObj = BeautifulSoup(url_phones, 'html.parser')
-    datosPhone = phonesObj.findAll("div", {"class":"caption"})
-    for datoPhone in datosPhone:
-        datoP.append({
-            "nombre": datoPhone.find("a", {"class":"title"}).text,
-            "caracteristicas": datoPhone.find("p", {"class":"description"}).text,
-            "valor":datoPhone.find("h4", {"class":"price"}).text
-         })
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+    options.add_argument("no-sandbox")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    urls = ["https://webscraper.io/test-sites/e-commerce/scroll/computers/laptops", "https://webscraper.io/test-sites/e-commerce/scroll/computers/tablets", "https://webscraper.io/test-sites/e-commerce/scroll/phones/touch"]
 
-    with open('phones.json', 'w') as file:
-        json.dump(datoP, file, indent=4, sort_keys=True)
-    with open('computers.json', 'w') as file:
-        json.dump(datoC, file, indent=4, sort_keys=True)
+    for url in urls:
+        driver.get(url)
+        print('url', url)
+        time.sleep(2)
+        screen_height = driver.execute_script("return window.screen.height;")
+        print(screen_height)
+        i=1
+        while True:
+            driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
+            i += 1
+            time.sleep(2)
+            scroll_height = driver.execute_script("return document.body.scrollHeight;")
+            print('hola',scroll_height)
+            if screen_height == scroll_height:
+                print('break while')
+                break
+            screen_height=scroll_height
 
 
-    return render_template('index.html', datoP=datoP, datoC=datoC)
+            allObj = BeautifulSoup(driver.page_source, 'html.parser')
+            dataProducts = allObj.findAll("div", {"class":"caption"})
+            if(url=="https://webscraper.io/test-sites/e-commerce/scroll/computers/laptops"):
+
+                datoL = list()
+                for dataProduct in dataProducts:
+                    datoL.append({
+                        "nombre": dataProduct.find("a", {"class":"title"}).text,
+                        "caracteristicas": dataProduct.find("p", {"class":"description"}).text,
+                        "valor":dataProduct.find("h4", {"class":"price"}).text
+                     })
+                with open('laptops.json', 'w') as file:
+                    json.dump(datoL, file, indent=4, sort_keys=True)
+
+
+            if(url=="https://webscraper.io/test-sites/e-commerce/scroll/phones/touch"):
+                datoP = list()
+                for dataProduct in dataProducts:
+                    datoP.append({
+                        "nombre": dataProduct.find("a", {"class":"title"}).text,
+                        "caracteristicas": dataProduct.find("p", {"class":"description"}).text,
+                        "valor":dataProduct.find("h4", {"class":"price"}).text
+                     })
+                with open('phones.json', 'w') as file:
+                    json.dump(datoP, file, indent=4, sort_keys=True)
+
+            if(url=="https://webscraper.io/test-sites/e-commerce/scroll/computers/tablets"):
+
+                datoT = list()
+                for dataProduct in dataProducts:
+                    datoT.append({
+                        "nombre": dataProduct.find("a", {"class":"title"}).text,
+                        "caracteristicas": dataProduct.find("p", {"class":"description"}).text,
+                        "valor":dataProduct.find("h4", {"class":"price"}).text
+                     })
+                with open('tablets.json', 'w') as file:
+                    json.dump(datoT, file, indent=4, sort_keys=True)
+
+
+    return render_template('index.html', datoP=datoP, datoL=datoL, datoT=datoT)
